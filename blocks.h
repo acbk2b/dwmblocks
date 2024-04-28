bd1b917c2dc849593182779af79e01efff5c5f78
Alex Becker <acbecker27@gmail.com>
2024-04-28 18:43:29 -0500

    Add battery and cpu blocks

--- blocks.h
+++ blocks.h
@@ -1,6 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "sb-battery",	30,		0},
+	{"CPU:", "sensors | awk '/Core 0/ { print $3 }'",	30,		0},
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %R%p'",					5,		0},
